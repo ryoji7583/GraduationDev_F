c8c70bed31539ca2194364982e12d4d1bb1301ac
ryoji7583 <ryoji.75830923@gmail.com>
2017-12-10 14:52:50 +0900

    文字列チェック機能追加
    文字列を各種類に分けてDBへ挿入する機能を追加した。

--- プログラム/URLSet.php
+++ プログラム/URLSet.php
@@ -86,12 +86,74 @@
                 $i++;
             }
 
+        $Overview = $PullRequestBody;
+
+        $start = '';
+        $end = '';
+        $src=$PullRequestBody;
+        if(mb_strpos($src,'【実行環境】') !== false){
+            $start = mb_strpos($src,'【実行環境】')+6;
+            $end = mb_strpos($src,'【このページでの内容】');
+            if($end){
+                $Environment = mb_substr($src, $start, $end-$start);
+            }else{
+                $end = mb_strpos($src,'【実装方法】');
+                if($end){
+                    $Environment = mb_substr($src, $start, $end-$start);
+                }else{
+                    $end = mb_strpos($src,'【参考ページ一覧】');
+                    if($end){
+                        $Environment = mb_substr($src, $start, $end-$start);   
+                    }else{
+                        $end = strlen($src);
+                        $Environment = mb_substr($src, $start, $end-$start);
+                    }
+                }
+            }
+            $start = '';
+            $end = '';
+        }
+        if(mb_strpos($src,'【このページでの内容】') !== false){
+            $start = mb_strpos($src,'【このページでの内容】')+11;
+            $end = mb_strpos($src,'【実装方法】');
+            if($end){
+                $Overview = mb_substr($src, $start, $end-$start);
+            }else{
+                $end = mb_strpos($src,'【参考ページ一覧】');
+                if($end){
+                    $Overview = mb_substr($src, $start, $end-$start);
+                }else{
+                    $end = strlen($src);
+                    $Overview = mb_substr($src, $start, $end-$start);
+                }
+            }
+            $start = '';
+            $end = '';
+        }
+        if(mb_strpos($src,'【実装方法】') !== false){
+            $start = mb_strpos($src,'【実装方法】')+6;
+            $end = mb_strpos($src,'【参考ページ一覧】');
+            if($end){
+                $Way = mb_substr($src, $start, $end-$start);
+            }else{
+                $end = strlen($src);
+                $Way = mb_substr($src, $start, $end-$start);
+            }
+            $start = '';
+            $end = '';
+        }
+        if(mb_strpos($src,'【参考ページ一覧】') !== false){
+            $start = mb_strpos($src,'【参考ページ一覧】')+9;
+            $end = strlen($src);
+            $Reference = mb_substr($src, $start, $end-$start);
+        }
+
         $stmt = $dbh -> prepare("INSERT INTO PRData VALUES (:RepositoryID, :PullRequestID, :Title, :Environment, :Overview, :Picture, :Way, :Reference)");
         $stmt->bindParam(':RepositoryID', $RepositoryID, PDO::PARAM_STR);
         $stmt->bindParam(':PullRequestID', $PullRequestID, PDO::PARAM_STR);
         $stmt->bindParam(':Title', $PullRequestTitle, PDO::PARAM_STR);
         $stmt->bindParam(':Environment', $Environment, PDO::PARAM_STR);
-        $stmt->bindParam(':Overview', $PullRequestBody, PDO::PARAM_STR);
+        $stmt->bindParam(':Overview', $Overview, PDO::PARAM_STR);
         $stmt->bindParam(':Picture', $Picture, PDO::PARAM_LOB);
         $stmt->bindParam(':Way', $Way, PDO::PARAM_STR);
         $stmt->bindParam(':Reference', $Reference, PDO::PARAM_STR);
@@ -100,8 +162,12 @@
         $PullRequestID='';
         $PullRequestTitle='';
         $PullRequestBody='';
+        $Environment = '';
+        $Picture     = '';
+        $Way         = '';
+        $Reference   = '';
+        $Overview    = '';
         }
-        echo '<PRE>';
     }else{
         echo "GitHubのURLを入力してください。";
     }
